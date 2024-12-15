9940afa7515d52db9b6b2d1438c633f79f13fe75
21f3003136 <21f3003136@ds.study.iitm.ac.in>
2024-12-15 16:15:04 +0000

    changes

--- autolysis.py
+++ autolysis.py
@@ -250,7 +250,8 @@ def save_readme(story, output_dir, img, file_path):
        for im in img:
             f.write(f"![{im['description']}]({im['filename']})\n")
 
-   backup_dir = os.path.join(os.getcwd(), 'README.md')
+   backup_dir = os.path.join(os.getcwd(),".csv" 'README.md')
+   shutil.copy(os.path.join(output_dir, 'README.md'), os.path.join(backup_dir, 'README.md'))
 
         
 def main(file_path):
